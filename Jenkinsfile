69ae0e464037022554b76ca04e53def482a43ab3
Sindre Rødseth Hansen <h103382@Sindres-MacBook-Pro.local>
2019-07-05 11:16:03 +0200

    alpine

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,7 +3,6 @@ pipeline {
     environment {
         client_secret_Q = credentials('client_secret_Q')
         VAULT_ADDR = 'https://vault.adeo.no'
-        //VAULT_ADDR = 'http://127.0.0.1:8200'
         proxy = 'http://webproxy-utvikler.nav.no:8088'
         https_proxy = 'http://webproxy-utvikler.nav.no:8088'
         no_proxy = 'adeo.no'
--- src/vaultToken.js
+++ src/vaultToken.js
@@ -1,5 +1,4 @@
 const vault = require('node-vault');
-//require('https').globalAgent.options.ca = require('ssl-root-cas').create();
 
 async function getToken() {
     ['VAULT_ADDR', 'AZURE_IAC_APPROLE_USR', 'AZURE_IAC_APPROLE_PSW'].forEach(env => {
@@ -13,7 +12,7 @@ async function getToken() {
         endpoint: process.env.VAULT_ADDR
     };
     let login_result = await vault(options).approleLogin({ role_id: process.env.AZURE_IAC_APPROLE_USR, secret_id: process.env.AZURE_IAC_APPROLE_PSW }).catch(err => console.error(err));
-    console.log(login_result);
+    //console.log(login_result);
     return login_result.auth.client_token;
 };
 module.exports.getToken = getToken
